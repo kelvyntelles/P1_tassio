7cb9e7a99823c10c121728497a02551de7859630
Kelvyn Telles <kelvyn.telles@gmail.com>
2021-09-23 13:52:47 -0300

    subindo o projeto

---
+++ src/main/java/com/br/prova/p1_tassio/P1TassioApplication.java
@@ -0,0 +1,13 @@
+package com.br.prova.p1_tassio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P1TassioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P1TassioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/prova/p1_tassio/P1TassioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.prova.p1_tassio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P1TassioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
